5396b2610dc3e74f6d339d1048117398060ea53c
talzaken <talzaken@gmail.com>
2024-01-02 15:42:24 -0500

    Add support for device name env variables & monitor inputs

--- multirecorder/av_recorder/av_recorder.py
+++ multirecorder/av_recorder/av_recorder.py
@@ -12,14 +12,41 @@ device_names = get_device_names()
 MICROPHONE_NAME = device_names["MICROPHONE_NAME"]
 SPEAKERS_NAME = device_names["SPEAKERS_NAME"]
 WEBCAM_NAME = device_names["WEBCAM_NAME"]
+MONITOR_NAME = device_names["MONITOR_NAME"]
 
-RECORD_SPEAKERS_COMMAND_ARGS = f'-f dshow -i audio="{SPEAKERS_NAME}"'
-RECORD_MICROPHONE_COMMAND_ARGS = f'-f dshow -i audio="{MICROPHONE_NAME}"'
-RECORD_MONITOR_COMMAND_ARGS = '-f gdigrab -framerate 30 -thread_queue_size 512 -i desktop'
-RECORD_WEBCAM_COMMAND_ARGS = '-f dshow -i video="Logi C270 HD WebCam"'
+RECORD_MICROPHONE_COMMAND_ARGS = f'-f dshow -i audio="{MICROPHONE_NAME}" -thread_queue_size 512'
+RECORD_SPEAKERS_COMMAND_ARGS = f'-f dshow -i audio="{SPEAKERS_NAME}" -thread_queue_size 512'
+RECORD_WEBCAM_COMMAND_ARGS = F'-f dshow -i video="{WEBCAM_NAME}" -framerate 32 -thread_queue_size 512'
+RECORD_MONITOR_COMMAND_ARGS = f'-f gdigrab -framerate 32 -thread_queue_size 512 -i {MONITOR_NAME}'
 
 
-def record_av(audio_source, video_source, filename="output_file", duration=None):
+def record_av(audio_source, video_source, picture_in_picture_video_source='', picture_in_picture_scale=1, filename="output_file", duration=None):
+    """
+    Record audio and video from multiple sources, using FFmpeg.
+
+    Parameters:
+    - audio_source (str): Specifies the audio input source. Accepted values are 'speakers' or 'microphone'.
+    - video_source (str): Specifies the video input source. Accepted values are 'monitor' or 'webcam'.
+    - picture_in_picture_video_source (str, optional): Specifies the picture-in-picture video source.
+      Accepted values are 'monitor' or 'webcam'. Default is an empty string (no picture-in-picture).
+    - picture_in_picture_scale (float, optional): Specifies the scale factor for the picture-in-picture video.
+      Default is 1.0 (no scaling).
+    - filename (str, optional): Specifies the output filename without extension. Default is "output_file".
+    - duration (float, optional): Specifies the recording duration in seconds. Default is None (no time limit).
+
+    Raises:
+    - AVException: If the input sources or their values are not recognized or specified incorrectly.
+
+    Note:
+    - The function uses FFmpeg to record audio and video, with optional picture-in-picture.
+    - The output file will be in AVI format with video codec libxvid and audio codec aac.
+
+    Example:
+    ```python
+    record_av(audio_source='speakers', video_source='monitor', picture_in_picture_video_source='webcam', duration=60)
+    ```
+
+    """
     try:
         if not audio_source or not video_source:
             raise AVException("Audio or video input source not specified")
@@ -38,14 +65,42 @@ def record_av(audio_source, video_source, filename="output_file", duration=None)
         else:
             raise AVException("video_source input not recognized. Accepted values include 'monitor' and 'webcam'")
 
-        duration_args = f'-t {duration}' if duration else ''
+        if picture_in_picture_video_source.lower() in {"m", "monitor", "d", "desktop"}:
+            picture_in_picture_video_command_args = RECORD_MONITOR_COMMAND_ARGS
+            scale_factor = 10 / picture_in_picture_scale
+            scale = f"iw/{scale_factor}:ih/{scale_factor}"
+
+        elif picture_in_picture_video_source.lower() in {"c", "camera", "w", "webcam"}:
+            picture_in_picture_video_command_args = RECORD_WEBCAM_COMMAND_ARGS
+            scale_factor = 1 / picture_in_picture_scale
+            scale = f"iw/{scale_factor}:ih/{scale_factor}"
+
+        elif not picture_in_picture_video_source:
+            picture_in_picture_video_command_args = ""
+
+        else:
+            raise AVException("picture_in_picture_video_source input not recognized. Accepted values include 'monitor' and 'webcam'")
+
+        filter_args = (
+            (
+                f'-filter_complex "[1:v]scale={scale} [overlay]'
+                f';[0:v][overlay]overlay=W-w-10:H-h-10[v01];[2:a]aformat=sample_fmts=fltp:channel_layouts=stereo '
+                '[a];[a][2:a]amerge=inputs=2[aout]" -map "[v01]" -map "[aout]"'
+            )
+            if picture_in_picture_video_command_args
+            else ''
+        )
+
+        duration_args = f"-t {duration}" if duration else ""
 
         command = (
             'ffmpeg',
             video_command_args,
+            picture_in_picture_video_command_args,
             audio_command_args,
             '-c:v libxvid -q:v 0',  # specifies video codec
             '-c:a aac -strict experimental -b:a 192k',  # specifies audio codec
+            filter_args,
             duration_args,
             f'{filename}.avi',
         )
@@ -62,8 +117,9 @@ def record_av(audio_source, video_source, filename="output_file", duration=None)
 if __name__ == "__main__":
     now = datetime.datetime.now().strftime("%Y%m%dT%H%M")
     record_av(
-        audio_source="s",
-        video_source="m",
+        audio_source="m",
+        video_source="w",
+        picture_in_picture_video_source="m",
         filename=f"recording_{now}",
-        duration=30
+        duration=5
     )
--- multirecorder/utils/get_device_names.py
+++ multirecorder/utils/get_device_names.py
@@ -1,11 +1,19 @@
 import subprocess
 import re
 import logging
+import os
 
 
-def get_device_names():
-    logging.info("Getting device names...")
-    command = "ffmpeg -list_devices true -f dshow -i dummy"
+def execute_ffmpeg_command(command):
+    """
+    Execute an FFmpeg command and return the result string.
+
+    Parameters:
+    - command (str): The FFmpeg command to be executed.
+
+    Returns:
+    - str or None: The result string if the command is successful, None otherwise.
+    """
     logging.debug(f"Running command: {command}")
 
     # Run the command and capture the output
@@ -16,32 +24,92 @@ def get_device_names():
         logging.error(f"Error: {result.stderr}")
         return None
 
-    result_str = str(result)
+    return str(result)
 
-    # Use regular expressions to extract device names
-    microphone_pattern = re.compile(r'] "(Microphone .*?)" \(audio\)')
-    speakers_pattern = re.compile(r'] "(Stereo Mix .*?)" \(audio\)')
-    webcam_pattern = re.compile(r'] "(.*?)" \(video\)')
 
-    microphone_match = microphone_pattern.search(result_str)
-    speakers_match = speakers_pattern.search(result_str)
-    webcam_match = webcam_pattern.search(result_str)
+def extract_device_name(pattern, result_str, env_variable, device_type):
+    """
+    Extract a device name from the result string using a specified regex pattern.
 
-    # Check if matches are found
-    if microphone_match and speakers_match and webcam_match:
-        microphone_name = microphone_match.group(1)
-        speakers_name = speakers_match.group(1)
-        webcam_name = webcam_match.group(1)
+    Parameters:
+    - pattern (re.Pattern): The compiled regex pattern for matching the device name.
+    - result_str (str): The result string obtained from FFmpeg command execution.
+    - env_variable (str or None): The value of the device name from environment variable.
+    - device_type (str): The type of the device (e.g., "Microphone", "Speakers", "Webcam").
 
-        logging.info(f"Microphone name: {microphone_name}")
-        logging.info(f"Speakers name: {speakers_name}")
-        logging.info(f"Webcam name: {webcam_name}")
-
-        return {
-            "MICROPHONE_NAME": microphone_name,
-            "SPEAKERS_NAME": speakers_name,
-            "WEBCAM_NAME": webcam_name
-        }
+    Returns:
+    - str: The extracted device name.
+    """
+    match = pattern.search(result_str)
+    if env_variable is None and match:
+        device_name = match.group(1)
+        logging.info(f"{device_type} name (from ffmpeg): {device_name}")
+        return device_name
     else:
-        logging.warning("Device names not found in the output.")
+        logging.info(f"{device_type} name (from environment variable): {env_variable}")
+        return env_variable
 
+
+def get_monitor_name():
+    """
+    Get the name of the monitor for recording.
+
+    The function checks if the environment variable 'MONITOR_NAME' is set. If set,
+    it returns the specified monitor name. If not set, it defaults to 'desktop'.
+
+    Returns:
+    - str: The name of the monitor for recording.
+    """
+    # Check if an environment variable is set for the monitor name
+    monitor_name = os.environ.get("MONITOR_NAME")
+
+    if monitor_name:
+        logging.info(f"Monitor name (from environment variable): {monitor_name}")
+        return monitor_name
+    else:
+        # Default monitor name when the environment variable is not set
+        default_monitor_name = "desktop"
+        logging.info(f"Monitor name not specified. Using default: {default_monitor_name}")
+        return default_monitor_name
+
+
+def get_device_names():
+    """
+    Get device names for Microphone, Speakers, and Webcam.
+
+    Returns:
+    - dict: A dictionary containing device names for Microphone, Speakers, and Webcam.
+    """
+    logging.info("Getting device names...")
+
+    # Check if environment variables are set for specific device names
+    microphone_name = os.environ.get("MICROPHONE_NAME")
+    speakers_name = os.environ.get("SPEAKERS_NAME")
+    webcam_name = os.environ.get("WEBCAM_NAME")
+    monitor_name = get_monitor_name()
+
+    # Check if any of the specific device names are missing
+    if None in (microphone_name, speakers_name, webcam_name):
+        logging.warning("Some or all device names not found in environment variables. Using ffmpeg commands.")
+
+        # Use ffmpeg commands to get the missing device names
+        command = "ffmpeg -list_devices true -f dshow -i dummy"
+        result_str = execute_ffmpeg_command(command)
+
+        if result_str is not None:
+            # Use regular expressions to extract device names
+            microphone_pattern = re.compile(r'] "(Microphone .*?)" \(audio\)')
+            speakers_pattern = re.compile(r'] "(Stereo Mix .*?)" \(audio\)')
+            webcam_pattern = re.compile(r'] "(.*?)" \(video\)')
+
+            # Update only the missing device names
+            microphone_name = extract_device_name(microphone_pattern, result_str, microphone_name, "Microphone")
+            speakers_name = extract_device_name(speakers_pattern, result_str, speakers_name, "Speakers")
+            webcam_name = extract_device_name(webcam_pattern, result_str, webcam_name, "Webcam")
+
+    return {
+        "MICROPHONE_NAME": microphone_name,
+        "SPEAKERS_NAME": speakers_name,
+        "WEBCAM_NAME": webcam_name,
+        "MONITOR_NAME": monitor_name,
+    }
